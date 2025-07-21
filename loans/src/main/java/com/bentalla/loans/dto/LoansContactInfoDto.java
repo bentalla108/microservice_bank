727467aeb253a7378f91a7facf3fe84b5a041a62
bentalla108 <tallambaye108@gmail.com>
2025-07-22 00:29:53 +0200

    Configure Rabbit mq .

--- loans/src/main/java/com/bentalla/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/bentalla/loans/dto/LoansContactInfoDto.java
@@ -1,4 +1,4 @@
-package com.bentalla.accounts.dto;
+package com.bentalla.loans.dto;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -7,10 +7,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.List;
 import java.util.Map;
 
-@ConfigurationProperties(prefix = "accounts")
+@ConfigurationProperties(prefix = "loans")
 @Getter
 @Setter
-public class AccountsContactInfoDto {
+public class LoansContactInfoDto {
     private String message;
     private Map<String, String> contactDetails;
     private List<String> onCallSupport;
